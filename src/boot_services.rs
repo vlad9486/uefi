db707a311a507c8223a213ce986dfd449a0fabc2
Vladislav Melnik <vlad9486@gmail.com>
2016-09-24 22:15:32 +0300

    add function dummies

--- src/boot_services.rs
+++ src/boot_services.rs
@@ -28,6 +28,45 @@ pub struct BootServices {
     signal_event: extern fn () -> (),
     close_event: extern fn () -> (),
     check_event: extern fn () -> (),
+
+    install_protocol_interface: extern fn () -> (),
+    reinstall_protocol_interface: extern fn () -> (),
+    uninstall_protocol_interface: extern fn () -> (),
+    handle_protocol: extern fn () -> (),
+    pchandle_protocol: extern fn () -> (),
+    register_protocol_notify: extern fn () -> (),
+    locate_handle: extern fn () -> (),
+    locate_device_path: extern fn () -> (),
+    install_configuration_table: extern fn () -> (),
+
+    load_image: extern fn () -> (),
+    start_image: extern fn () -> (),
+    exit: extern fn () -> (),
+    unload_image: extern fn () -> (),
+    exit_boot_services: extern fn () -> (),
+
+    get_next_monotonic_count: extern fn () -> (),
+    stall: extern fn () -> (),
+    set_watchdog_timer: extern fn () -> (),
+
+    connect_controller: extern fn () -> (),
+    disconnect_controller: extern fn () -> (),
+
+    open_protocol: extern fn () -> (),
+    close_protocol: extern fn () -> (),
+    open_protocol_information: extern fn () -> (),
+
+    protocols_per_handle: extern fn () -> (),
+    locate_handle_buffer: extern fn () -> (),
+    locate_protocol: extern fn () -> (),
+    install_multiple_protocol_interfaces: extern fn () -> (),
+    uninstall_multiple_protocol_interfaces: extern fn () -> (),
+
+    calculate_crc32: extern fn () -> (),
+
+    copy_mem: extern fn () -> (),
+    set_mem: extern fn () -> (),
+    create_event_ex: extern fn () -> ()
 }
 
 impl BootServices {
--- src/runtime_services.rs
+++ src/runtime_services.rs
@@ -6,6 +6,24 @@ pub const SIGNATURE: u64 = 0x56524553544e5552;
 pub struct RuntimeServices {
     header: Header,
 
+    get_time: extern fn () -> (),
+    set_time: extern fn () -> (),
+    get_wakeup_time: extern fn () -> (),
+    set_wakeup_time: extern fn () -> (),
+
+    set_virtual_address_map: extern fn () -> (),
+    convert_pointer: extern fn () -> (),
+
+    get_variable: extern fn () -> (),
+    get_next_variable_name: extern fn () -> (),
+    set_variable: extern fn () -> (),
+
+    get_next_high_monotonic_count: extern fn () -> (),
+    reset_system: extern fn () -> (),
+
+    update_capsule: extern fn () -> (),
+    query_capsule_capabilities: extern fn () -> (),
+    query_variable_info: extern fn () -> ()
 }
 
 impl RuntimeServices {
